397ac3c7e48176936a74badfce727303c99739f9
Oliver Trosien <otrosien@epages.com>
2016-05-27 08:04:33 +0200

    only build on linux

--- travis_build.sh
+++ travis_build.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
 set -ev
-if [ "$TRAVIS_OS_NAME" == "osx" ]; then
-	if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
-		echo "Build on MacOSX: Pull Request"
-		./mvnw clean verify
-	else
-		echo "Skipping build on MacOSX for standard commit"
-	fi
-else
-	echo "Build on Linux"
-	./mvnw clean verify || exit 1
-	./gradlew clean build || exit 1
-fi 
+echo "Build on Linux"
+./mvnw clean verify || exit 1
+./gradlew clean build || exit 1
